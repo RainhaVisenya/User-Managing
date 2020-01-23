5cbcc3efdbe4361e93f4c35d753640c99c5e9c0e
Geovana <gcn@certi.org.br>
2020-01-23 14:47:55 -0300

    [C-35] JS Events

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ var admin = document.querySelector("#exampleInputAdmin");
 
 
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index){
 
+    if (field.name == "gender") {
 
+        if (field.checked)  {
+            user.gender
+        }
 
+    } else {
+        user[field.name] = field.value; 
+
+    }
+
+});
+
+console.log(user);
+
+document.querySelector("button").forEach(function() {
+
+    this.
 });
