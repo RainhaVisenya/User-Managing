d0f8894885781361d8dedbe26465d4b24bacf66e
Geovana <gcn@certi.org.br>
2020-01-28 09:45:08 -0300

    [C-50] DataSet

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -23,8 +23,10 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
-                (content)=>{
+                (content) => {
 
                     values.photo = content;
 
@@ -85,8 +87,15 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name > -1 && !field.value)) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
         if (field.name == "gender") {
 
@@ -105,6 +114,11 @@ class UserController {
     }
 
 });
+
+    if (!isValid) {
+        return false;
+    }
+
     return new User(
         user.name, 
         user.gender, 
@@ -122,6 +136,8 @@ class UserController {
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser)
     
          tr.innerHTML = `
         <tr>
@@ -138,6 +154,29 @@ class UserController {
 `;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+}
+
+updateCount() {
+
+    let numberUsers = 0;
+
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr=>{
+
+        numberUsers++;
+
+        let user = JSON.parse(tr.dataset.user);
+
+        if (user._admin) numberAdmin++;
+
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
 }
 
 }
